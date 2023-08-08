df26e1bd43fbfae364cc85632bfc773824eab7ba
rmaWhrdl <wwscan3@gmail.com>
2023-08-08 16:59:51 +0900

    test : splitAndSum_null_또는_빈문자() 테스트 완료

---
+++ src/main/java/StringAddCalculator.java
@@ -0,0 +1,23 @@
+public class StringAddCalculator {
+    public static int splitAndSum(String in) {
+        int sum = 0;
+        String[] strArr;
+
+        if(isNull(in)) {
+            return 0;
+        }
+
+        return sum;
+    }
+
+    private static boolean isNull(String in) {
+        if(in == null || in.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    private static int stoi(String str) {
+        return Integer.parseInt(str);
+    }
+}
